ec64d44600e849210f010383090b383fae644200
Nanditha-98 <nandithamenon.mec@gmail.com>
2020-12-01 01:01:26 +0530

    Updated databse

--- covidSafe/settings.py
+++ covidSafe/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'covidSafe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
