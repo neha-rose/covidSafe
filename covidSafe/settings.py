0c52b51eb77a8cbb03574fe0961fd78a04882845
Nanditha-98 <nandithamenon.mec@gmail.com>
2020-12-01 00:21:19 +0530

    Database Engine updated

--- covidSafe/settings.py
+++ covidSafe/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS=["../static"]
+
+
 
 # Application definition
 
@@ -75,8 +78,7 @@ WSGI_APPLICATION = 'covidSafe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
     }
 }
 
