829cf96795850680635ed8c515df3e61f895b5e1
Anjali-Joy <57320107+Anjali-Joy@users.noreply.github.com>
2020-12-01 18:32:57 +0530

    views.py changed

--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
 def welcomepage(request):
     return render(request, "main/welcome.html", {})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=email, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
