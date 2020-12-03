2fde04bf1d684edfc9089951dc402f2bf949a72d
Anjali-Joy <57320107+Anjali-Joy@users.noreply.github.com>
2020-12-03 23:24:06 +0530

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -7,7 +7,7 @@ class RegisterForm(UserCreationForm):
     shop_name = forms.CharField(max_length=30, required=True, help_text='Enter name of the shop')
     email = forms.EmailField(max_length=254, help_text='Enter a valid email address.')
     adminpw = forms.CharField(widget= forms.PasswordInput)
-    }    
+ 
     class Meta:
         model = User
         fields = ( 'shop_name', 'email', 'password1', 'password2', 'adminpw')
