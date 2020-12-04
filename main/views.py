53f52c12f3f68fd00c8ecc77844a2fe9ff5b13db
Neha Rose George <neharosegeorge@gmail.com>
2020-12-05 03:35:17 +0530

    updated

--- main/views.py
+++ main/views.py
@@ -14,11 +14,14 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            messages.success(request, f"New account created!")
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect('main:home')
+        else:
+            messages.error(request, f"Your password can't be too similar to your other personal information or a commonly used password.")
+            messages.error(request, f"Your password must contain atleast 8 characters and can't be entirely numeric.")
     else:
-        messages.error(request, f"Oops! Try again")
         form = RegisterForm()
     return render(request, 'main/register.html', {'form': form})
 
@@ -27,7 +30,7 @@ def login_req(request):
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
