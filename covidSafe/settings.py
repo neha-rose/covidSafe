72dd0bced9760f4e2be8fa02a81c2b27e23bae55
Neha Rose George <neharosegeorge@gmail.com>
2020-12-05 16:42:45 +0530

    updated alerts

--- covidSafe/settings.py
+++ covidSafe/settings.py
@@ -17,11 +17,11 @@ import os
 from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
+        messages.DEBUG: 'secondary',
+        messages.INFO: 'info',
+        messages.SUCCESS: 'success',
+        messages.WARNING: 'warning',
+        messages.ERROR: 'danger',
  }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
